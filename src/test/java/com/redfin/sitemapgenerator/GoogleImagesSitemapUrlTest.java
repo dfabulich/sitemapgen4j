329e36e8bc7d8e061e4c2d91e19e849ab1a1f9fc
veserratol <victor.serrato@globant.com>
2014-08-06 12:00:47 -0500

    Update format.

--- src/test/java/com/redfin/sitemapgenerator/GoogleImagesSitemapUrlTest.java
+++ src/test/java/com/redfin/sitemapgenerator/GoogleImagesSitemapUrlTest.java
@@ -39,6 +39,7 @@ public class GoogleImagesSitemapUrlTest extends TestCase {
                 "title", "http://www.example.com/licence.html");
         url.addImage(image);
         wsg.addUrl(url);
+        
         String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
                 + "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" "
                 + "xmlns:image=\"http://www.google.com/schemas/sitemap-image/1.1\" >\n"
