04dfbfd2f1109b09f24558f1bebc061658f8a71e
twb <twb@foreach.be>
2017-05-23 16:08:03 +0200

    Additional test and uncomment plugin

--- src/test/java/com/redfin/sitemapgenerator/GoogleNewsSitemapUrlTest.java
+++ src/test/java/com/redfin/sitemapgenerator/GoogleNewsSitemapUrlTest.java
@@ -81,6 +81,34 @@ public class GoogleNewsSitemapUrlTest extends TestCase {
 		String sitemap = writeSingleSiteMap(wsg);
 		assertEquals(expected, sitemap);
 	}
+
+	public void testGenres() throws Exception {
+		W3CDateFormat dateFormat = new W3CDateFormat(Pattern.SECOND);
+		dateFormat.setTimeZone(W3CDateFormat.ZULU);
+		wsg = GoogleNewsSitemapGenerator.builder("http://www.example.com", dir)
+				.dateFormat(dateFormat).build();
+		GoogleNewsSitemapUrl url = new GoogleNewsSitemapUrl.Options("http://www.example.com/index.html", new Date(0), "Example Title", "The Example Times", "en")
+				.genres("persbericht")
+				.build();
+		wsg.addUrl(url);
+		String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+				"<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" xmlns:news=\"http://www.google.com/schemas/sitemap-news/0.9\" >\n" +
+				"  <url>\n" +
+				"    <loc>http://www.example.com/index.html</loc>\n" +
+				"    <news:news>\n" +
+				"      <news:publication>\n" +
+				"        <news:name>The Example Times</news:name>\n" +
+				"        <news:language>en</news:language>\n" +
+				"      </news:publication>\n" +
+				"      <news:genres>persbericht</news:genres>\n" +
+				"      <news:publication_date>1970-01-01T00:00:00Z</news:publication_date>\n" +
+				"      <news:title>Example Title</news:title>\n" +
+				"    </news:news>\n" +
+				"  </url>\n" +
+				"</urlset>";
+		String sitemap = writeSingleSiteMap(wsg);
+		assertEquals(expected, sitemap);
+	}
 	
 	private String writeSingleSiteMap(GoogleNewsSitemapGenerator wsg) {
 		List<File> files = wsg.write();
